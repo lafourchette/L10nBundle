6c22570ba2a612031e3b6c176aaaa8ee87bc30d3
Cyril Otal <cotal@lafourchette.com>
2014-04-25 16:48:32 +0200

    Complete Provider

--- Business/L10nProvider.php
+++ Business/L10nProvider.php
@@ -2,6 +2,8 @@
 
 namespace L10nBundle\Business;
 
+use L10nBundle\Entity\L10nResource;
+
 use L10nBundle\Exception\ResourceNotFoundException;
 
 use L10nBundle\Manager\L10nManagerInterface;
@@ -19,31 +21,103 @@ class L10nProvider
     protected $l10nManager;
 
     /**
-     * @param L10nManagerInterface $l10nManager
+     *
+     * @var mixed
      */
-    public function __construct(L10nManagerInterface $l10nManager)
+    protected $defaultLocalisation;
+
+    /**
+     *
+     * @var string
+     */
+    protected $defaultLocale;
+
+    /**
+     * @param L10nManagerInterface $l10nManager
+     * @param mixed $defaultLocalisation
+     * @param mixed $defaultLocale
+     */
+    public function __construct(L10nManagerInterface $l10nManager, $defaultLocalisation, $defaultLocale)
     {
         $this->l10nManager = $l10nManager;
+        $this->defaultLocalisation = $defaultLocalisation;
+        $this->defaultLocale = $defaultLocale;
     }
 
     /**
      *
+     * @return mixed
+     */
+    public function getDefaultLocalisation()
+    {
+        return $this->defaultLocalisation;
+    }
+
+    /**
+     *
+     * @param mixed $defaultLocalisation
+     * @return L10nResource
+     */
+    public function setDefaultLocalisation($defaultLocalisation)
+    {
+        $this->defaultLocalisation = $defaultLocalisation;
+        return $this;
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getDefaultLocale()
+    {
+        return $this->defaultLocale;
+    }
+
+    /**
+     *
+     * @param string $defaultLocale
+     * @return L10nResource
+     */
+    public function setDefaultLocale($defaultLocale)
+    {
+        $this->defaultLocale = $defaultLocale;
+        return $this;
+    }
+
+    /**
+     * Return a localised value
      * @param mixed $idResource
      * @param mixed $idLocalisation
      * @param string $locale
+     * @return string
      * @throws ResourceNotFoundException
      */
-    public function getL10n($idResource, $idLocalisation, $locale = null)
+    public function getL10n($idResource, $idLocalisation = null, $locale = null)
     {
+        if (!$idLocalisation) {
+            $idLocalisation = $this->defaultLocalisation;
+        }
+        if (!$locale) {
+            $locale = $this->defaultLocale;
+        }
         $resource = $this->l10nManager->getL10nResource($idResource, $idLocalisation);
         if (!$resource) {
             throw new ResourceNotFoundException(sprintf('Resource not found for idResource %s and idLocalisation %s', $idResource, $idLocalisation));
         }
         $valueList = $resource->getValueList();
 
-        // stuff with $locale
+        $value ='';
+        if (count($valueList) === 1) {
+            $value = reset($valueList);
+        } else {
+            if (isset($valueList[$locale])) {
+                $value = $valueList[$locale];
+            } elseif (isset($valueList[$this->defaultLocale])) {
+                $value = $valueList[$this->defaultLocale];
+            }
+        }
 
-        // ⇒ defaultLocale ?
-
+        return $value;
     }
+
 }
--- Tests/Business/L10nProviderTest.php
+++ Tests/Business/L10nProviderTest.php
@@ -16,36 +16,68 @@ class L10nProviderTest extends \PHPUnit_Framework_TestCase
 
 
 
-    function testGetL10n()
+    /**
+     *
+     * @dataProvider provideData
+     */
+    public function testGetL10n($idResource, $idLocalisation, $idLocalisationAskedToManager, $locale, $defaultLocalisation, $defaultLocale, $l10nResource, $expected)
     {
-        $idResource = 'key';
-        $idLocalisation = 'France';
-
-        $l10nResource = new L10nResource();
-
 
         $l10nManager = $this->getMock('L10nBundle\Manager\L10nManagerInterface', array('getL10nResource', 'setL10nResource'), array(), '', false);
         $l10nManager
             ->expects($this->once())
             ->method('getL10nResource')
-            ->with($idResource, $idLocalisation)
+            ->with($idResource, $idLocalisationAskedToManager)
             ->will($this->returnValue($l10nResource))
         ;
 
-        $l10nProvider = new L10nProvider($l10nManager);
-        $l10nProvider->getL10n($idResource, $idLocalisation);
+        $l10nProvider = new L10nProvider($l10nManager, $defaultLocalisation, $defaultLocale);
+        $value = $l10nProvider->getL10n($idResource, $idLocalisation, $locale);
+        $this->assertEquals($expected, $value);
 
     }
 
+    public function provideData()
+    {
+        $return = array();
 
+        $idResource = 'key';
+        $idLocalisation = 'France';
+        $defaultLocalisation = 'Japan';
+        $defaultLocale = 'fr-BE';
+        $locale = 'sl-SI';
+
+        $l10nResource = new L10nResource();
+        $valueList = array();
+        $value = 'plop';
+        $valueList[$locale] = $value;
+        $l10nResource->setValueList($valueList);
+
+        // I18N Value
+        $return[] = array($idResource, $idLocalisation, $idLocalisation, $locale, $defaultLocalisation, $defaultLocale, $l10nResource, $value);
+
+        $l10nResource->setValueList(array($value));
+
+        // non I18N value
+        $return[] = array($idResource, $idLocalisation, $idLocalisation, $locale, $defaultLocalisation, $defaultLocale, $l10nResource, $value);
+
+        $l10nResource->setValueList(array($defaultLocale => $value));
+
+        // test fallbacks
+        $return[] = array($idResource, null, $defaultLocalisation, null, $defaultLocalisation, $defaultLocale, $l10nResource, $value);
+
+        return $return;
+    }
 
     /**
      * @expectedException \L10nBundle\Exception\ResourceNotFoundException
      */
-    function testGetL10nWithResourceNotFoundException()
+    public function testGetL10nWithResourceNotFoundException()
     {
         $idResource = 'key';
         $idLocalisation = 'France';
+        $defaultLocalisation = 'Japan';
+        $defaultLocale = 'fr-BE';
 
         $l10nManager = $this->getMock('L10nBundle\Manager\L10nManagerInterface', array('getL10nResource', 'setL10nResource'), array(), '', false);
         $l10nManager
@@ -55,7 +87,7 @@ class L10nProviderTest extends \PHPUnit_Framework_TestCase
             ->will($this->returnValue(null))
         ;
 
-        $l10nProvider = new L10nProvider($l10nManager);
+        $l10nProvider = new L10nProvider($l10nManager, $defaultLocalisation, $defaultLocale);
         $l10nProvider->getL10n($idResource, $idLocalisation);
 
     }
