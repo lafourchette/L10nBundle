22c25e26d4aa6ab217ebbe9db71adbf791616bd9
Cyril Otal <cotal@lafourchette.com>
2014-06-11 15:26:45 +0200

    add Export feature

--- Exception/ResourceNotFoundException.php
+++ Exception/ResourceNotFoundException.php
@@ -2,14 +2,6 @@
 
 namespace L10nBundle\Exception;
 
-
 class ResourceNotFoundException extends \Exception
 {
-    /**
-     * @param string $message
-     */
-    public function __construct($message = null)
-    {
-        parent::__construct($message);
-    }
 }
